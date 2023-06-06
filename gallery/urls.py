da6514b6ae4b39871c29629b9a3a2f81c823b793
mateusxis <mateusx@poli.ufrj.br>
2023-06-06 13:29:01 +0100

    feat: create page search

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from gallery.views import index, image
+from gallery.views import index, image, search
 
 urlpatterns = [
     path('', index, name='home'),
     path('image/<int:picture_id>', image, name='image'),
+    path('search', search, name='search'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -10,5 +10,11 @@ def image(request, picture_id):
     picture = get_object_or_404(Picture, pk=picture_id) 
     return render(request, 'gallery/image.html', {"picture": picture})
 
-def image(request):
-    return render(request, 'gallery/image.html')
+def search(request):
+    pictures = Picture.objects.order_by("-date_picture").filter(published=True)
+
+    if "term" in request.GET:
+        term = request.GET["term"]
+        if term:
+            pictures = pictures.filter(name__icontains=term)
+    return render(request, 'gallery/search.html', {"pictures": pictures})
